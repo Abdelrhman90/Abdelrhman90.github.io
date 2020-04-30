88641a3a63809e42a7f5b981d685ac2bdaddfd0e
Abdelrahman <abdelrhmanmkting@gmail.com>
2020-04-30 12:31:24 +0200

    initial commit

---
+++ social-media-dashboard/index.js
@@ -0,0 +1,22 @@
+const toggler = document.querySelector('input')
+const squares = document.querySelectorAll('.square')
+const overviewSquares = document.querySelectorAll('.overview-square')
+
+toggler.addEventListener('click', e => {
+    
+    document.querySelector('.heading').classList.toggle('light-heading')
+    document.body.classList.toggle('light-theme')
+    document.querySelector('.hero-container').classList.toggle('light-hero')
+    document.querySelector('.slider').classList.toggle('light-slider')
+
+    squares.forEach(square => {
+        square.classList.toggle('light-square')
+    })
+
+    overviewSquares.forEach(square => {
+        square.classList.toggle('light-overview-square')
+    })
+    
+    
+    
+})
