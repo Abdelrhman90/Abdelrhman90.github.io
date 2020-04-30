911981612921be51dfb98817a536af787ac8bf8d
Abdelrahman <abdelrhmanmkting@gmail.com>
2020-04-30 13:13:44 +0200

    intial commit

--- social-challenge/index.js
+++ social-challenge/index.js
@@ -3,7 +3,8 @@ const squares = document.querySelectorAll('.square')
 const overviewSquares = document.querySelectorAll('.overview-square')
 
 toggler.addEventListener('click', e => {
-    
+
+    document.querySelector('.dark').classList.toggle('light')
     document.querySelector('.heading').classList.toggle('light-heading')
     document.body.classList.toggle('light-theme')
     document.querySelector('.hero-container').classList.toggle('light-hero')
